e4ce591887999369cc4725bb617a57b6ae979880
info@vaktija.ba <info@vaktija.ba>
2019-08-04 22:01:02 +0200

    Removed x-powered-by

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(compression());
+app.disable('x-powered-by')
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
