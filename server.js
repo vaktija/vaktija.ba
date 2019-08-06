c3192975c383166b32409a0a03d5666d19e62b60
info@vaktija.ba <info@vaktija.ba>
2019-08-06 20:18:26 +0200

    Revert Prerender

--- server.js
+++ server.js
@@ -4,15 +4,7 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-
-const prerender = require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN);
-prerender.crawlerUserAgents.push('googlebot');
-prerender.crawlerUserAgents.push('bingbot');
-prerender.crawlerUserAgents.push('yandex');
-
 app.use(compression());
-app.use(prerender);
-
 app.disable('x-powered-by')
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
