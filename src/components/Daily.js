085c3df3609dae1cbab9fe6422ee9d77c67a9ab6
info@vaktija.ba <info@vaktija.ba>
2019-10-19 15:50:55 +0200

    Played with Hooks notifications disabled/commented

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Grid, Row, Col, Glyphicon } from "react-bootstrap";
-// import moment from "moment";
-import moment from "moment-hijri";
+import moment from "moment";
+import momenth from "moment-hijri";
 import 'moment-timezone';
 import "moment-duration-format";
 import "moment/locale/bs";
 // import ReactGA from "react-ga";
 import Cookies from 'universal-cookie';
 import slugify from "slugify";
-import ReactNotifications from 'react-browser-notifications';
-import uuidv4 from "uuid/v4";
+// import ReactNotifications from 'react-browser-notifications';
+// import uuidv4 from "uuid/v4";
 import Helmet from "react-helmet";
 import { locations, locationsDative, vakatNames } from '../data/vaktija.json';
 import { daily } from "../api/vaktija/index.mjs";
@@ -28,7 +28,6 @@ import Locations from './Locations';
 import Footer from './Footer';
 
 const cookies = new Cookies();
-
 // ReactGA.initialize("UA-9142566-1");
 
 moment.updateLocale("bs", {
@@ -49,27 +48,10 @@ moment.updateLocale("bs", {
     weekdaysShort: ["ned", "pon", "uto", "sri", "čet", "pet", "sub"]
 });
 
-class Daily extends Component {
+function Daily({ locationProps = 77, root }) {
 
-    // toggleTheme = () => {
-
-    //     const { theme } = this.state;
-
-    //     if (theme === 'light') {
-    //         document.body.classList.remove('light');
-    //         document.body.classList.add('dark');
-    //         this.setState({ theme: 'dark' });
-
-    //     } else if (theme === 'dark') {
-    //         document.body.classList.remove('dark');
-    //         document.body.classList.add('light');
-    //         this.setState({ theme: 'light' });
-    //     }
-    // }
-
-    nextVakat = () => {
-
-        const nextVakatPosition = daily(this.localization()).vakat.map((v, i) => ({ pos: i, active: moment().tz("Europe/Sarajevo").isSameOrBefore(moment(v, 'HH:mm').tz("Europe/Sarajevo")) }))
+    function nextVakat() {
+        const nextVakatPosition = daily(localization()).vakat.map((v, i) => ({ pos: i, active: moment().tz("Europe/Sarajevo").isSameOrBefore(moment(v, 'HH:mm').tz("Europe/Sarajevo")) }))
 
         if (nextVakatPosition.filter(n => n.active === true).length) {
             return nextVakatPosition.filter(n => n.active === true)[0].pos
@@ -78,86 +60,76 @@ class Daily extends Component {
         }
     }
 
-    showNotifications = () => {
+    function showNotifications() {
         if (this.n.supported()) this.n.show();
     }
 
-    handleClick = (event) => {
-        window.focus();
-        this.n.close(event.target.tag);
-    }
+    // function handleClick(event) {
+    //     window.focus();
+    //     this.n.close(event.target.tag);
+    // }
 
-    openNav = () => {
+    function openNav() {
         document.getElementById("sidenav").style.width = "100%";
     }
 
-    closeNav = (e) => {
+    function closeNav(e) {
         e.preventDefault()
         document.getElementById("sidenav").style.width = "0";
     }
 
-    tick = () => {
-
-        const { vaktija } = this.state;
+    function tick() {
         const clock = moment().tz("Europe/Sarajevo").format();
         const notifs = vaktija.map((v, i) => moment(v, "HH:mm").tz("Europe/Sarajevo").subtract(15, "m").format());
-        const nextVakatPosition = daily(this.localization()).vakat.map((v, i) => ({ pos: i, active: moment().tz("Europe/Sarajevo").isSameOrBefore(moment(v, 'HH:mm').tz("Europe/Sarajevo")) }))
+        const nextVakatPosition = daily(localization()).vakat.map((v, i) => ({ pos: i, active: moment().tz("Europe/Sarajevo").isSameOrBefore(moment(v, 'HH:mm').tz("Europe/Sarajevo")) }))
 
-        this.setState({
-            currentMoment: moment().tz("Europe/Sarajevo"),
-            date: [
-                moment().tz("Europe/Sarajevo").format('ddd, D. MMMM'),
-                moment().tz("Europe/Sarajevo").format('YYYY'),
-                moment().tz("Europe/Sarajevo").format("iD. iMMMM iYYYY").toLowerCase()
-            ],
-            vaktija: daily(this.localization()).vakat
-        });
+        setCurrentMoment(moment().tz("Europe/Sarajevo"));
+        setVaktija(daily(localization()).vakat);
+        setDate([
+            moment().tz("Europe/Sarajevo").format('ddd, D. MMMM'),
+            moment().tz("Europe/Sarajevo").format('YYYY'),
+            momenth().tz("Europe/Sarajevo").format("iD. iMMMM iYYYY").toLowerCase()
+        ])
 
         if (notifs.includes(clock)) {
-            this.setState({ position: notifs.indexOf(clock) })
-            this.showNotifications();
+            showNotifications();
         }
 
         if (nextVakatPosition.filter(n => n.active === true).length) {
-            this.setState({ nextVakatPosition: nextVakatPosition.filter(n => n.active === true)[0].pos })
+            setNextVakatPosition(nextVakatPosition.filter(n => n.active === true)[0].pos);
+
         } else {
-            this.setState({ nextVakatPosition: 6 })
+            setNextVakatPosition(6);
         }
 
         if (moment().isBetween(moment(vaktija[1], "HH:mm"), moment(vaktija[4], "HH:mm"))) {
-            this.setState({ theme: 'light' })
+            setTheme('light');
             document.body.classList.remove('dark');
             document.body.classList.add('light');
         } else {
-            this.setState({ theme: 'dark' })
+            setTheme('dark');
             document.body.classList.remove('light');
             document.body.classList.add('dark');
         }
     };
 
-    localization = () => {
-        if (this.props.root === undefined && cookies.get("location") !== undefined) {
+    function localization() {
+        if (root === undefined && cookies.get("location") !== undefined) {
             return cookies.get("location");
         } else {
-            return this.props.locationProps;
+            return locationProps;
         }
     }
 
-    state = {
-        currentMoment: moment().tz("Europe/Sarajevo"),
-        locationState: this.localization(),
-        date: [
-            moment().tz("Europe/Sarajevo").format('ddd, D. MMMM'),
-            moment().tz("Europe/Sarajevo").format('YYYY'),
-            moment().tz("Europe/Sarajevo").format("iD. iMMMM iYYYY").toLowerCase()
-        ],
-        vaktija: daily(this.localization()).vakat,
-        nextVakatPosition: this.nextVakat(),
-        theme: moment().isBetween(moment(daily(this.localization()).vakat[1], "HH:mm"), moment(daily(this.localization()).vakat[4], "HH:mm")) ? 'light' : 'dark'
-    }
+    const [currentMoment, setCurrentMoment] = useState(moment().tz("Europe/Sarajevo"));
+    const [locationState] = useState(localization());
+    const [vaktija, setVaktija] = useState(daily(localization()).vakat);
+    const [nextVakatPosition, setNextVakatPosition] = useState(nextVakat());
+    const [theme, setTheme] = useState(moment().isBetween(moment(daily(localization()).vakat[1], "HH:mm"), moment(daily(localization()).vakat[4], "HH:mm")) ? 'light' : 'dark');
+    const [date, setDate] = useState([moment().tz("Europe/Sarajevo").format('ddd, D. MMMM'), moment().tz("Europe/Sarajevo").format('YYYY'), momenth().tz("Europe/Sarajevo").format("iD. iMMMM iYYYY").toLowerCase()]);
 
-    componentDidMount() {
-        const { theme } = this.state;
+    useEffect(() => {
+        const interval = setInterval(() => tick(), 1000);
 
         if (theme === 'light') {
             document.body.classList.remove('dark');
@@ -167,125 +139,110 @@ class Daily extends Component {
             document.body.classList.add('dark');
         }
 
-        // ReactGA.pageview(window.location.pathname + window.location.search);
-        this.timerID = setInterval(() => this.tick(), 1000);
-
-        if (!this.props.root) {
-            cookies.set("location", this.props.locationProps, { path: '/', domain: '.vaktija.ba', expires: moment().add(1, "y").tz("Europe/Sarajevo").toDate() });
+        if (!root) {
+            cookies.set("location", locationProps, { path: '/', domain: '.vaktija.ba', expires: moment().add(1, "y").tz("Europe/Sarajevo").toDate() });
         }
-    }
 
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+        return () => clearInterval(interval);
+    });
 
-    render() {
-
-        const { currentMoment, date, vaktija, locationState, nextVakatPosition, theme } = this.state;
-
-        return (
-            <>
-                <Helmet>
-                    <link
-                        rel="canonical"
-                        href={`https://vaktija.ba/${slugify(
-                            locations[locationState],
-                            {
-                                replacement: "-",
-                                remove: null,
-                                lower: true,
-                            },
-                        )}`}
-                    />
-                    <meta
-                        name="description"
-                        content={`Vaktija za ${locationsDative[locationState]}, ${date[0]} ${date[1]} / ${date[2]}${
-                            vakatNames.map((vakatName, index) => ` ${vakatName} ${vaktija[index]}`)
-                            }. Preuzmite oficijelne Android, iOS (iPhone, iPad) i Windows mobilne aplikacije, namaz, salat, džuma, sehur, ramazan, iftar, teravija, takvim, bosna i hercegovina, sandžak`}
-                    />
+    return <>
+        <Helmet>
+            <link
+                rel="canonical"
+                href={`https://vaktija.ba/${slugify(
+                    locations[locationState],
                     {
-                        theme === 'light' &&
-                        <meta name="theme-color" content="#ffffff" />
-                    }
-                    {
-                        theme === 'dark' &&
-                        <meta name="theme-color" content="#1e2227" />
-                    }
-                    <title>{`${locations[locationState]} · Vaktija`}</title>
-                </Helmet>
-                <ReactNotifications
-                    onRef={ref => (this.n = ref)}
-                    title={`${vakatNames[nextVakatPosition]} je za 15 minuta`}
-                    body={`${locationsDative[locationState]}, ${date[0]} ${date[1]} / ${date[2]}`}
-                    icon={"icon.png"}
-                    tag={uuidv4()}
-                    interaction="true"
-                    onClick={event => this.handleClick(event)}
-                />
-                <Grid>
-                    <Row>
-                        <Col xs={6}>
-                            <Link to="/">
-                                {
-                                    theme === 'dark' &&
-                                    <>
-                                        <img className="hidden-xs hidden-sm" src={LogoLight} alt="vaktija.ba" height="48"></img>
-                                        <img className="hidden-md hidden-lg" src={IconLight} alt="vaktija.ba" height="32"></img>
-                                    </>
-                                }
-                                {
-                                    theme === 'light' &&
-                                    <>
-                                        <img className="hidden-xs hidden-sm" src={LogoDark} alt="vaktija.ba" height="48"></img>
-                                        <img className="hidden-md hidden-lg" src={IconDark} alt="vaktija.ba" height="32"></img>
-                                    </>
-                                }
-                            </Link>
-                        </Col>
-                        <Col className="text-right" xs={6}>
-                            <Glyphicon glyph="map-marker" onClick={this.openNav} className={`glyphicon-${theme}`} />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
-                            <Counter vakatTime={vaktija[nextVakatPosition]} theme={theme} />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12} sm={12} md={12} lg={12}>
-                            <Location theme={theme} location={locationState} />
-                            <CurrentDate theme={theme} date={date} location={locationState} />
-                        </Col>
-                    </Row>
-                    <Row className="text-center">
+                        replacement: "-",
+                        remove: null,
+                        lower: true,
+                    },
+                )}`}
+            />
+            <meta
+                name="description"
+                content={`Vaktija za ${locationsDative[locationState]}, ${date[0]} ${date[1]} / ${date[2]}${
+                    vakatNames.map((vakatName, index) => ` ${vakatName} ${vaktija[index]}`)
+                    }. Preuzmite oficijelne Android, iOS (iPhone, iPad) i Windows mobilne aplikacije, namaz, salat, džuma, sehur, ramazan, iftar, teravija, takvim, bosna i hercegovina, sandžak`}
+            />
+            {
+                theme === 'light' &&
+                <meta name="theme-color" content="#ffffff" />
+            }
+            {
+                theme === 'dark' &&
+                <meta name="theme-color" content="#1e2227" />
+            }
+            <title>{`${locations[locationState]} · Vaktija`}</title>
+        </Helmet>
+        {/* <ReactNotifications
+            onRef={ref => (this.n = ref)}
+            title={`${vakatNames[nextVakatPosition]} je za 15 minuta`}
+            body={`${locationsDative[locationState]}, ${date[0]} ${date[1]} / ${date[2]}`}
+            icon={"icon.png"}
+            tag={uuidv4()}
+            interaction="true"
+            onClick={event => handleClick(event)}
+        /> */}
+        <Grid>
+            <Row>
+                <Col xs={6}>
+                    <Link to="/">
                         {
-                            vakatNames.map((vakatName, index) => <Col key={vaktija[index]} xs={12} sm={12} md={12} lg={2}>
-                                <VakatTime theme={theme} vakatName={vakatName} vakatTime={vaktija[index]} highlight={nextVakatPosition === index ? true : false} />
-                                <RelativeTime currentMoment={currentMoment} theme={theme} vakatTime={vaktija[index]} />
-                            </Col>
-                            )
+                            theme === 'dark' &&
+                            <>
+                                <img className="hidden-xs hidden-sm" src={LogoLight} alt="vaktija.ba" height="48"></img>
+                                <img className="hidden-md hidden-lg" src={IconLight} alt="vaktija.ba" height="32"></img>
+                            </>
                         }
-                    </Row>
-                    <Row>
-                        <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
-                            <br />
-                            <br />
-                            <Stores theme={theme} />
-                        </Col>
-                    </Row>
-                </Grid>
-                <div id="sidenav" className="sidenav">
-                    <a href="true" className="closebtn" onClick={(e) => this.closeNav(e)}>&times;</a>
-                    <Locations />
-                </div>
-                <br />
-                <Footer theme={theme} />
-            </>
-        )
-    }
+                        {
+                            theme === 'light' &&
+                            <>
+                                <img className="hidden-xs hidden-sm" src={LogoDark} alt="vaktija.ba" height="48"></img>
+                                <img className="hidden-md hidden-lg" src={IconDark} alt="vaktija.ba" height="32"></img>
+                            </>
+                        }
+                    </Link>
+                </Col>
+                <Col className="text-right" xs={6}>
+                    <Glyphicon glyph="map-marker" onClick={openNav} className={`glyphicon-${theme}`} />
+                </Col>
+            </Row>
+            <Row>
+                <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
+                    <Counter vakatTime={vaktija[nextVakatPosition]} theme={theme} />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={12} md={12} lg={12}>
+                    <Location theme={theme} location={locationState} />
+                    <CurrentDate theme={theme} date={date} location={locationState} />
+                </Col>
+            </Row>
+            <Row className="text-center">
+                {
+                    vakatNames.map((vakatName, index) => <Col key={vaktija[index]} xs={12} sm={12} md={12} lg={2}>
+                        <VakatTime theme={theme} vakatName={vakatName} vakatTime={vaktija[index]} highlight={nextVakatPosition === index ? true : false} />
+                        <RelativeTime currentMoment={currentMoment} theme={theme} vakatTime={vaktija[index]} />
+                    </Col>
+                    )
+                }
+            </Row>
+            <Row>
+                <Col className="text-center" xs={12} sm={12} md={12} lg={12}>
+                    <br />
+                    <br />
+                    <Stores theme={theme} />
+                </Col>
+            </Row>
+        </Grid>
+        <div id="sidenav" className="sidenav">
+            <a href="true" className="closebtn" onClick={(e) => closeNav(e)}>&times;</a>
+            <Locations />
+        </div>
+        <br />
+        <Footer theme={theme} />
+    </>
 }
 
-Daily.defaultProps = {
-    locationProps: 77,
-}
 export default Daily;
