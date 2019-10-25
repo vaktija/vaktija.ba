e7204196cb36b142d5162c4fb7c5651da0adb260
info@vaktija.ba <info@vaktija.ba>
2019-10-25 17:32:17 +0200

    Preserve theme preference (via cookie) and stop automatic theming

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,6 +1,13 @@
 import React, { createContext, useState } from 'react';
+import Cookies from 'universal-cookie';
+import moment from "moment";
+import 'moment-timezone';
+import "moment-duration-format";
+import "moment/locale/bs";
 
 export const ThemeContext = createContext();
+
+const cookies = new Cookies();
 
 function ThemeContextProvider(props) {
 
@@ -8,15 +15,28 @@ function ThemeContextProvider(props) {
     const [theme, setTheme] = useState('light');
 
     const initTheme = (theme) => {
-        if (theme === 'dark') {
-            setTheme('dark');
-            document.body.classList.remove('light');
-            document.body.classList.add('dark');
 
-        } else if (theme === 'light') {
-            setTheme('light');
-            document.body.classList.remove('dark');
-            document.body.classList.add('light');
+        if (cookies.get("theme") === 'dark' || cookies.get("theme") === 'light') {
+            setTheme(cookies.get("theme"));
+            if (cookies.get("theme") === 'dark') {
+                document.body.classList.remove('light');
+                document.body.classList.add('dark');
+            } else if (cookies.get("theme") === 'light') {
+                document.body.classList.remove('dark');
+                document.body.classList.add('light');
+            }
+            setAutomaticTheme(false);
+        } else {
+            if (theme === 'dark') {
+                setTheme('dark');
+                document.body.classList.remove('light');
+                document.body.classList.add('dark');
+
+            } else if (theme === 'light') {
+                setTheme('light');
+                document.body.classList.remove('dark');
+                document.body.classList.add('light');
+            }
         }
     }
 
@@ -26,12 +46,13 @@ function ThemeContextProvider(props) {
             setAutomaticTheme(false);
             document.body.classList.remove('light');
             document.body.classList.add('dark');
-
+            cookies.set("theme", theme, { path: '/', expires: moment().add(1, "y").tz("Europe/Sarajevo").toDate() });
         } else if (theme === 'light') {
             setTheme('light');
             setAutomaticTheme(false);
             document.body.classList.remove('dark');
             document.body.classList.add('light');
+            cookies.set("theme", theme, { path: '/', expires: moment().add(1, "y").tz("Europe/Sarajevo").toDate() });
         }
     }
 
