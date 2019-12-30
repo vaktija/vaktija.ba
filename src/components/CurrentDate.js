2e1873e6ca527891b6655b66d188e6a030407dee
info@vaktija.ba <info@vaktija.ba>
2019-12-30 04:13:06 +0100

    Fixed year in pdf files - dynamically

--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -3,7 +3,7 @@ import { locations } from '../data/vaktija.json';
 
 function CurrentDate({ date, theme, location }) {
     return <p className="text-center">
-        <i className={`date-${theme}`}>{date[0]} <a className={`year-${theme}`} href={`download/pdf/${locations[location]}_2019.pdf`}>{date[1]}</a> / {date[2]}</i>
+        <i className={`date-${theme}`}>{date[0]} <a className={`year-${theme}`} href={`download/pdf/${locations[location]}_${date[1]}.pdf`}>{date[1]}</a> / {date[2]}</i>
     </p >
 }
 
