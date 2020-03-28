f67382ecad4f91eebdb6a7f2fd7ea519a41669bf
info@vaktija.ba <info@vaktija.ba>
2020-03-28 13:32:57 +0100

    Classsic css import for Stores styling

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -7,30 +7,27 @@ function Stores({ theme }) {
     <div className="text-center">
       <a
         aria-label="Official Android App"
-        className={`store-${theme}`}
+        className={`store store-${theme}`}
         target="_blank"
         rel="noopener noreferrer"
-        style={{ padding: 10 }}
         href="https://play.google.com/store/apps/details?id=ba.vaktija.android"
       >
         <FontAwesomeIcon icon={["fab", "android"]} size="3x" />
       </a>
       <a
         aria-label="Official iOS App"
-        className={`store-${theme}`}
+        className={`store store-${theme}`}
         target="_blank"
         rel="noopener noreferrer"
-        style={{ padding: 10 }}
         href="https://itunes.apple.com/us/app/vaktija.ba/id1095343967?ls=1&mt=8"
       >
         <FontAwesomeIcon icon={["fab", "apple"]} size="3x" />
       </a>
       <a
         aria-label="Official Windows App"
-        className={`store-${theme}`}
+        className={`store store-${theme}`}
         target="_blank"
         rel="noopener noreferrer"
-        style={{ padding: 10 }}
         href="https://www.microsoft.com/en-us/store/apps/vaktijaba/9nblggh5lc4p"
       >
         <FontAwesomeIcon icon={["fab", "windows"]} size="3x" />
