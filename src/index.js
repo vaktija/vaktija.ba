f8162586253d0a9f93eb862b931dffdd9e6321fa
info@vaktija.ba <info@vaktija.ba>
2020-05-31 20:29:10 +0200

    Remove core-js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import "core-js/stable";
-import "regenerator-runtime/runtime";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
