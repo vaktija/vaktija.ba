c9848b2f8bed2623cfb9cba6d40809745d5895f6
info@vaktija.ba <info@vaktija.ba>
2020-06-08 18:33:31 +0200

    FontAwesomeIcon to JS

---
+++ src/icons/Windows.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+function SvgWindowsBrands(props) {
+  return (
+    <svg
+      aria-hidden="true"
+      data-prefix="fab"
+      data-icon="windows"
+      className="windows-brands_svg__svg-inline--fa windows-brands_svg__fa-windows windows-brands_svg__fa-w-14"
+      viewBox="0 0 448 512"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z"
+      />
+    </svg>
+  );
+}
+
+export default SvgWindowsBrands;
